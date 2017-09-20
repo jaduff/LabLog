2dbc5ebaedf72a26ba2d1d84c6b7417d9c21fe77
James Duff <jaoduff@gmail.com>
2017-09-20 16:51:20 +0800

    Dummied up data to help with creating the view

---
+++ src/LabLog/Controllers/TeacherController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LabLog.Models;
+
+namespace LabLog.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room(string id)
+        {
+            ViewData["Message"] = "Room Page.";
+
+            RoomModel room = new RoomModel();
+            room.Name = id;
+            room.createComputers(20);
+
+            ViewData["RoomName"] = id;
+            ViewData["Computers"] = room.Computers;
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
