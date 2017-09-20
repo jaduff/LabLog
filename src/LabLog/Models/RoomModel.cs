e5d09d768c4e7be95e35b4cab94d4d9a13c7b5e5
James Duff <jaoduff@gmail.com>
2017-09-20 16:52:01 +0800

    Changed Computers from array to List (for IEnumerable). Implemented logic to create computers.

--- src/LabLog/Models/RoomModel.cs
+++ src/LabLog/Models/RoomModel.cs
@@ -1,11 +1,21 @@
 using System;
 using LabLog.Models;
+using System.Collections.Generic;
 namespace LabLog
 {
     class RoomModel
     {
         public string Name {get; set;}
-        public ComputerModel[] Computers {get; set;}
+        public List<ComputerModel> Computers {get; set;}
+
+        public void createComputers(int computerCount) {
+            for (int i =0; i==computerCount; i++){
+               ComputerModel computer = new ComputerModel();
+               computer.ComputerNumber = i+1;
+               Computers.Add(computer);
+            }
+
+        }
         /* some way of representing room layout? Image blob? URL to image? something fancy like a floor plan using drag and drop? */
         
     }
