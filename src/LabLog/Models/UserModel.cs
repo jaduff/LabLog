b0c77bb564dc26561fb675936aa8210216ed7e50
James Duff <jaoduff@gmail.com>
2017-09-25 19:45:10 +0800

    Login interface begun.

---
+++ src/LabLog/Models/UserModel.cs
@@ -0,0 +1,9 @@
+namespace LabLog.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Message { get; set; }
+    } 
+}
