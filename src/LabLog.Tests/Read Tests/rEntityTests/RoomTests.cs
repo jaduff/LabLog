0922795ccfb34d8a516b4c1441c50aa692023d3f
James Duff <jaoduff@gmail.com>
2017-09-30 10:28:11 +0800

    Modified folder structure to make sense for namespaces. Begun work on read tests

---
+++ src/LabLog.Tests/Read Tests/rEntityTests/RoomTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using CheetahTesting;
+using Xunit;
+
+namespace LabLog.ReadTests.rEntityTests
+{
+    class RoomTests
+    {
+        [Fact]
+        public async Task RoomCanBeCreatedFromEvent()
+        {
+            await CTest<ReadRoomContext>
+                .Given(C => { })
+                .When(i => i.ReceiveRoomCreatedEvent())
+                .Then(t => t.RoomIsInstantiated())
+                .And(t => t.RoomHasId())
+                .ExecuteAsync();
+        }
+
+    }
+}
