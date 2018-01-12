58bff3845ca5d5b0019a041dcce7673cd0864a53
James Duff <jaoduff@gmail.com>
2018-01-12 14:28:01 +0800

    Added damage model to computer model

--- src/LabLog/Models/DamageModel.cs
+++ src/LabLog/Models/DamageModel.cs
@@ -1,7 +1,7 @@
 using System;
 namespace LabLog.Models
 {
-    class DamageModel
+    public class DamageModel
     {
         public int DamageID {get; set;}
         public string SerialNumber {get; set;}
